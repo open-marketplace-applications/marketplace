775e25f76d23fc18c8f2686c21dc81d41f3a2040
huhn511 <sehe89@gmail.com>
2020-03-28 03:22:29 +0100

    add shop create papge and create shop function

---
+++ components/CreateShop.vue
@@ -0,0 +1,86 @@
+<template>
+  <div v-loading="loading" class="shop-details-form-container">
+    <el-alert v-if="published" title="Shop is not published yet!" type="warning">
+      <p>You can add your shop details and publish it!</p>
+    </el-alert>
+
+    <el-form ref="form" :model="form" class="form-container">
+      <p>You can add your shop details and publish it to the tangle!</p>
+
+      <div class="shop-details-form-main-container">
+        <el-row>
+          <el-col :span="24">
+            <el-form-item label="Name:">
+              <el-input
+                v-model="form.name"
+                :rows="1"
+                type="text"
+                class="shop-name"
+                autosize
+                placeholder="Your shop name"
+              />
+            </el-form-item>
+
+            <el-form-item label="Location">
+                <!-- <LocationChooseMap @update="updateIac"/> -->
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </div>
+      <el-button @click="onCancel" type="warning">Cancel</el-button>
+      <el-button style="margin-left: 10px;" type="success" @click="onSubmit">Create</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import { createShop } from './shop/index'
+// import LocationChooseMap from "./LocationChooseMap";
+export default {
+  name: "CreateShop",
+  components: { },
+  props: {
+    published: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      loading: false,
+      shop: {},
+      form: {
+        name: ""
+      }
+    };
+  },
+  created() {
+    // fetch shop from database
+    // let shop_string = localStorage.getItem("shop") || "{}";
+    // this.shop = JSON.parse({shop_string});
+  },
+  methods: {
+    onSubmit: async function() {
+      // save it to mam!
+        console.log("onSubmit")
+        this.loading = true;
+          if(this.iac && this.showMap) {
+              this.form.iac = this.iac
+        }
+        let shop = await createShop(this.form);
+
+        console.log("shop", shop)
+        // save new shop secrets in database
+        const parsed = JSON.stringify(shop);
+        //localStorage.setItem("shop", parsed);
+        this.loading = false;        
+    },
+    onCancel() {
+      this.$router.push("/");
+    },
+    updateIac(newIac) {
+      this.form.iac = newIac
+    }
+  }
+};
+</script>
---
+++ pages/shop.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="shop">
+    <Navigation />
+    <h1>Create your shop</h1>
+    <client-only placeholder="Loading...">
+      <!-- this component will only be rendered on client-side -->
+        <CreateShop />
+    </client-only>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navigation from "../components/Navigation.vue";
+import CreateShop from "../components/CreateShop.vue";
+import Footer from "../components/Footer";
+
+export default {
+  components: { Navigation, CreateShop, Footer }
+};
+</script>
+
+<style lang="scss">
+
+#shop {
+    margin: 150px 20px;
+}
+</style>
