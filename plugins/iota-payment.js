c7c5d24297a53c5bb6ab60c082d94cbac3d69763
huhn511 <sehe89@gmail.com>
2020-03-29 01:28:44 +0100

    fix url

--- plugins/iota-payment.js
+++ plugins/iota-payment.js
@@ -4,5 +4,5 @@ import Vue from 'vue'
 import IotaPayment from 'vue-iota-payment';
 
 export default ({ store }) => {
-    Vue.use(IotaPayment, { store, url: process.env.PROJECTS_URL, path: '/iotapay' });
+    Vue.use(IotaPayment, { store, url: process.env.CITY_URL, path: '/iotapay' });
 }
