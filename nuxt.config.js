eae6bb7eea32e5a7e652e80fe0c9e85a15ded238
huhn511 <sehe89@gmail.com>
2020-04-01 23:01:06 +0200

    fix github org move

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,21 +4,16 @@ const config = require('./config.json')
 
 const title = process.env.CITY_TITLE ? process.env.CITY_TITLE : config.title;
 const description = process.env.CITY_DESCRIPTION ? process.env.CITY_DESCRIPTION : config.description;
-const gh_repo_name = process.env.GH_REPO_NAME ? process.env.GH_REPO_NAME : config.gh_repo_name;
 const latitude = process.env.CITY_LATITUDE ? process.env.CITY_LATITUDE : config.location.latitude;
 const longitude = process.env.CITY_LONGITUDE ? process.env.CITY_LONGITUDE : config.location.longitude;
 
 module.exports = {
   mode: 'universal',
-  router: {
-    base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/' + gh_repo_name + '/' : ''
-  },
-  mode: 'universal',
   /*
   ** Headers of the page
   */
   head: {
-    title: title + ' Marktplatz - Open Marketplace Application}',
+    title: title + ' Marktplatz - Open Marketplace Applications',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
